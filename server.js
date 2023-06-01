370bc72fc80303845b96cf8d5783eb031baa2337
Marisol Valenzuela <marisol_cabrera18@hotmail.com>
2023-05-31 21:34:54 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
+// MIDDLEWARE
+const breadsControllers = require('./controllers/breads_controllers')
+app.use('/breads', breadsControllers)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 //routes:
 app.get('/', (req, res) => {
     res.send('Welcome to the World of Bread!')
 })
-
-const breadsControllers =require('./controllers/breads_controllers.js')
-app.use('/breads', breadsControllers
-)
 
 app.listen(PORT, ()=> {
     console.log('listening on port: ', PORT);
