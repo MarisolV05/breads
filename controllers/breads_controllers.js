7affdc9689d6cd2a02f67a243d6cc4e0a92269a1
marisol Valenzuela <marisolvalenzuela@MacBook-Air.local>
2023-06-05 04:37:19 -0700

    add

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -1,20 +1,27 @@
 
 const express = require('express')
 const breads = express.Router()
-const Breads = require('../models/bread.js')
+const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
     {
-        breads: Breads
+        breads: Bread
     })
     // res.send(Bread)
 
 })
+//new
+
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+    //res.send(Bread[req.params.arrayIndex])
+    res.render('Show', {
+        breads: Bread[req.params.arrayIndex]
+    })
   })
   
 module.exports = breads
