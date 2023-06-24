713b7ef0bafec1b4a8abf001e72ffd21737f86cd
marisol Valenzuela <marisolvalenzuela@MacBook-Air.local>
2023-06-24 10:54:01 -0700

    update

--- server.js
+++ server.js
@@ -18,20 +18,25 @@ mongoose.connect(process.env.MONGO_URI,
     ()=>{console.log('connected to mongoDB: ', process.env.MONGO_URI)})
   
 // Middleware:
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-  
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 // bakers 
 const bakersController = require('./controllers/bakers_controller.js')
 app.use('/bakers', bakersController)
-  
+
+constPORT = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
+
 // 404 Page
 app.get('*', (req, res) => {
     res.send('404')
